335b627b40346c9f8aad6a6c2914753987c71a02
Paul Elden <paulelden11@gmail.com>
2022-01-15 17:24:10 -0500

    Longest commit ever lawl I refactored everything and updated the app to be a UI app. No idea at this point where the previous commit left off.

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,33 @@
-function getRandomInt(max) {
+// variables
+let playerScore = 0; 
+let computerScore = 0;
+
+const playButtons = document.querySelector("#playButtons");
+const pScore = document.querySelector("#player");
+const cScore = document.querySelector("#computer");
+
+const rockButton = document.querySelector('#rockButton');
+rockButton.addEventListener('click', () => { playRound('rock', computerPlay()) });
+
+const paperButton = document.querySelector('#paperButton');
+paperButton.addEventListener('click', () => { playRound('paper', computerPlay()) });
+
+const scissorsButton = document.querySelector('#scissorsButton');
+scissorsButton.addEventListener('click', () => { playRound('scissors', computerPlay()) });
+
+let cValue = document.querySelector("#cValue").innerHTML;
+let pValue = document.querySelector("#pValue").innerHTML;
+
+const body = document.querySelector("body"); // will use this to add result as child element
+
+// functions
+
+function getRandomInt(max) {    // this is required to get a true random num
     return Math.floor(Math.random() * max);
   }
   
 
-function computerPlay(num=3) {
+function computerPlay(num=3) {  // convert random int (1-3) into play value
     randomNum = getRandomInt(num)
     if (randomNum === 1) {
         return "rock";
@@ -16,73 +40,94 @@ function computerPlay(num=3) {
     }
 }
 
+function updateScore() {
+    document.querySelector("#pValue").innerHTML = playerScore;
+    document.querySelector("#cValue").innerHTML = computerScore;
+}
+
+function playAgain() {
+    let replay = prompt("Wanna play again? Y/N");
+    if (replay === null) {
+        // pass
+    }
+    else if (replay.toUpperCase() === 'N') {
+        // pass
+    }
+    else if (replay.toUpperCase() === 'Y') {
+        location.reload();
+    }
+    else { playAgain() }
+}
+
+function isWinner() {
+    if (playerScore === 5 || computerScore === 5) {
+        alert(`Game over! Final Score:\nPlayer:     ${playerScore}\nComputer:   ${computerScore}`);
+        playAgain();
+    }
+}
+// this contains the comparison logic to determine who wins a round
 function playRound(playerSelection, computerSelection) {
 
-    if (playerSelection.toLowerCase() === "rock" && computerSelection === "paper" ) {
-        return 1;    // lose
-    }
-    else if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors" ) {
-        return 3;    // win
-    }
-    else if (playerSelection.toLowerCase() === "rock" && computerSelection === "rock" ) {
-        return 2;    // tie
-    }
-    else if (playerSelection.toLowerCase() === "paper" && computerSelection === "rock" ) {
-        playerScore++;
-        return 3;
-    }
-    else if (playerSelection.toLowerCase() === "paper" && computerSelection === "scissors" ) {
+    if (playerSelection === "rock" && computerSelection === "paper" ) {
+        isWinner();
         computerScore++;
-        return 1;
+        console.log('You lose!');
+        updateScore();
     }
-    else if (playerSelection.toLowerCase() === "paper" && computerSelection === "paper" ) {
-        return 2;
-    }
-    else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "rock" ) {
-        computerScore++;
-        return 1;
-    }
-    else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper" ) {
+    else if (playerSelection === "rock" && computerSelection === "scissors" ) {
+        isWinner();
         playerScore++;
-        return 3;
+        console.log('You win!');
+        updateScore();
     }
-    else if (playerSelection.toLowerCase() === "scissors" && computerSelection === "scissors" ) {
-        return 2;
+    else if (playerSelection === "rock" && computerSelection === "rock" ) {
+        isWinner();
+        console.log('Tie!');
+        updateScore();
+    }
+    else if (playerSelection === "paper" && computerSelection === "rock" ) {
+        isWinner();
+        playerScore++;
+        console.log('You win!');
+        updateScore();
+    }
+    else if (playerSelection === "paper" && computerSelection === "scissors" ) {
+        isWinner();
+        computerScore++;
+        console.log('You lose!');
+        updateScore();
+    }
+    else if (playerSelection === "paper" && computerSelection === "paper" ) {
+        isWinner();
+        console.log('Tie!');
+        updateScore();
+    }
+    else if (playerSelection === "scissors" && computerSelection === "rock" ) {
+        computerScore++;
+        console.log('You lose!');
+        updateScore();
+        isWinner();
+    }
+    else if (playerSelection === "scissors" && computerSelection === "paper" ) {
+        isWinner();
+        playerScore++;
+        console.log('You win!') 
+        updateScore();
+    }
+    else if (playerSelection === "scissors" && computerSelection === "scissors" ) {
+        isWinner();
+        console.log('Tie!');
+        updateScore();
     }
 }
 
 function game() {
-    let playerScore = 0; 
-    let computerScore = 0;
-    let roundCounter = 0;
-    console.log("Time to play Rock, Paper, Scissors!")
-    console.log("Ready?");
-    while (roundCounter < 6) {
-        let playerSelect = prompt("Rock, paper or scissors?");
-        const computerSelect = computerPlay();
-        const result = playRound(playerSelect, computerSelect);
-
-        if (result === 1) {
-            computerScore++;
-            console.log("You lose!");
-        }
-        else if (result === 3) {
-            playerScore++;
-            console.log("You win!");
-        }
-        else {
-            console.log("Tie!");
-        }
-        console.log(`The score is now ${playerScore} to ${computerScore}.`)
-        roundCounter++;
+    alert('Time to play Rock Paper Scissors! First to five wins. \n\nAre you ready?')
+    if ( playerScore === 5 ) {
+        alert('player');
     }
-
-    console.log("Game over! Final score: ");
-    console.log(`You: ${playerScore}`);
-    console.log(`Computer: ${computerScore}`);
-
     
-
 }
 
+// main
 game();
